aaeeac2abbd3a292ec246138373485d25a781996
Burhan <buran.udachan@rightbraininfotech.in>
2021-03-05 17:25:31 +0530

    user soft delete done

--- src/main/java/com/eai/idss/repository/UserRepository.java
+++ src/main/java/com/eai/idss/repository/UserRepository.java
@@ -1,10 +1,14 @@
 package com.eai.idss.repository;
 
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.eai.idss.model.User;
+
+import java.util.List;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
@@ -12,5 +16,9 @@ public interface UserRepository extends MongoRepository<User, String> {
     User findByUserName(String name);
     
     long deleteByUserName(String name);
+
+    Page<User> findByIsActive(boolean isActive, Pageable page);
+
+
 
 }
---
+++ src/main/java/com/eai/idss/service/UserService.java
@@ -0,0 +1,7 @@
+package com.eai.idss.service;
+
+import java.util.List;
+
+public interface UserService {
+    public List<String> deleteByUserName(List<String> userNames);
+}
