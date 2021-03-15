aeb4d9b8a036b0a585f55b710de48dfe3de41894
unknown <swarup.shridhar@blucognition.com>
2021-03-15 14:34:45 +0530

    user password sending email updated

--- src/main/java/com/eai/idss/util/SendEmail.java
+++ src/main/java/com/eai/idss/util/SendEmail.java
@@ -21,17 +21,17 @@ public class SendEmail {
 	public static void sendmail(String toAddress,String pass) throws AddressException, MessagingException, IOException {
 		   Properties props = new Properties();
 		   props.put("mail.smtp.auth", "true");
-		   props.put("mail.smtp.starttls.enable", "true");
+//		   props.put("mail.smtp.starttls.enable", "true");
 		   props.put("mail.smtp.host", "mail.mpcb.gov.in");
 		   props.put("mail.smtp.port", "25");
 		   
 		   Session session = Session.getInstance(props, new javax.mail.Authenticator() {
 		      protected PasswordAuthentication getPasswordAuthentication() {
-		         return new PasswordAuthentication("idss.support@mpcb.giv.in", "idss#2021");
+		         return new PasswordAuthentication("idss.support@mpcb.gov.in", "Idss#2021");
 		      }
 		   });
 		   Message msg = new MimeMessage(session);
-		   msg.setFrom(new InternetAddress("idss.support@mpcb.giv.in", false));
+		   msg.setFrom(new InternetAddress("idss.support@mpcb.gov.in", false));
 
 		   msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toAddress));
 		   msg.setSubject("New User Registration");
@@ -43,10 +43,6 @@ public class SendEmail {
 
 		   Multipart multipart = new MimeMultipart();
 		   multipart.addBodyPart(messageBodyPart);
-		   MimeBodyPart attachPart = new MimeBodyPart();
-
-		   attachPart.attachFile("/var/tmp/image19.png");
-		   multipart.addBodyPart(attachPart);
 		   msg.setContent(multipart);
 		   Transport.send(msg);   
 		}
